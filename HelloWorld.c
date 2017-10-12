e5855789021c6e685a79745ba9db92f76d6719f6
Mahajanayuk <ayuk_mahajan@infosys.com>
2017-10-12 16:50:21 +0530

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int32_t main(void) {
-    printf("Hello World!\n");
-    return 1;
+class HelloWorld {
+    
+    int32_t main(void) {
+         printf("Hello World!\n");
+         return 1;
+    }
 }
